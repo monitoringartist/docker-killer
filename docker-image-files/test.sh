25ef958f63ed3f1f523bfe8f32d11e21f9841e6d
Jan Garaj <jan.garaj@gmail.com>
2016-04-30 22:57:08 +0100

    Colours

--- docker-image-files/test.sh
+++ docker-image-files/test.sh
@@ -4,31 +4,31 @@
 export TIMEOUT=${TIMEOUT:-60}
 export NETBOMB=${NETBOMB:-"iperf -c iperf.scottlinux.com -t ${TIMEOUT} -i 1 -p 5201 -u"}
 
-green='\e[0;32m'
-yellow='\e[0;33m'
-red='\e[0;31m'
-blue='\e[0;34m'
-endColor='\e[0m'
+export green='\e[0;32m'
+export yellow='\e[0;33m'
+export red='\e[0;31m'
+export blue='\e[0;34m'
+export endColor='\e[0m'
 
 help() {
-  echo -en "${red}WARNING: IT IS NOT GUARANTEED THAT YOUR SYSTEM/CONTAINERS WILL SURVIVE THIS KILLER TESTING! DO NOT USE THIS IMAGE UNLESS YOU REALLY KNOW WHAT ARE YOU DOING!${endColor}"
-  echo -en "${yellow}Tests included in this image, such as: cpubomb, membomb, netbomb, forkbomb, ...${endColor}"
-  echo -en "${yellow}Use 'all' or name of the particular test, e.g.:${endColor}" 
-  echo -en "${yellow}docker run --rm -ti --privileged -v /:/rootfs --oom-kill-disable monitoringartist/docker-killer cpubomb${endColor}"
+  echo -e "${red}WARNING: IT IS NOT GUARANTEED THAT YOUR SYSTEM/CONTAINERS WILL SURVIVE THIS KILLER TESTING! DO NOT USE THIS IMAGE UNLESS YOU REALLY KNOW WHAT ARE YOU DOING!${endColor}"
+  echo -e "${yellow}Tests included in this image, such as: cpubomb, membomb, netbomb, forkbomb, ...${endColor}"
+  echo -e "${yellow}Use 'all' or name of the particular test, e.g.:${endColor}" 
+  echo -e "${yellow}docker run --rm -ti --privileged -v /:/rootfs --oom-kill-disable monitoringartist/docker-killer cpubomb${endColor}"
 }
 export -f help
 
 forkbomb() {
-  echo -en "${red}forkbomb - duration ${TIMEOUT}s${endColor}"
-  echo -en "${yellow}Test: classic bash shell fork bomb${endColor}"
+  echo -e "${red}forkbomb - duration ${TIMEOUT}s${endColor}"
+  echo -e "${yellow}Test: classic bash shell fork bomb${endColor}"
   :(){ :|:& };:
   # TODO
 }
 export -f forkbomb
 
 cpubomb() {
-  echo -en "${red}cpubomb - duration ${TIMEOUT}s${endColor}"
-  echo -en "${yellow}Test: excessive CPU utilization - one proces per processor with empty cycles${endColor}"
+  echo -e "${red}cpubomb - duration ${TIMEOUT}s${endColor}"
+  echo -e "${yellow}Test: excessive CPU utilization - one proces per processor with empty cycles${endColor}"
   top -b -n${TIMEOUT} -d1 | grep "^CPU:" &
   #top -b -n${TIMEOUT} -d1 | grep "^Load average:" &      
   (
@@ -42,44 +42,44 @@ cpubomb() {
 export -f cpubomb
 
 membomb() {
-  echo -en "${red}membomb - duration ${TIMEOUT}s${endColor}"
-  echo -en "${yellow}Test: excessive memory utilization - bash variable with RAM+Swap size${endColor}"
+  echo -e "${red}membomb - duration ${TIMEOUT}s${endColor}"
+  echo -e "${yellow}Test: excessive memory utilization - bash variable with RAM+Swap size${endColor}"
   top -b -n${TIMEOUT} -d1 | grep "^Mem:" & 
   /membomb.bin
 }
 export -f membomb
 
 netbomb() {
-  echo -en "${red}netbomb - duration ${TIMEOUT}s${endColor}"
-  echo -en "${yellow}Test: excessive network utilization - iperf against public iperf server${endColor}"  
+  echo -e "${red}netbomb - duration ${TIMEOUT}s${endColor}"
+  echo -e "${yellow}Test: excessive network utilization - iperf against public iperf server${endColor}"  
   eval $NETBOMB
 }
 export -f netbomb
 
 die() {
-  echo -en "${red}die${endColor}"
-  echo -en "${yellow}Test: exit container with exit code 1${endColor}"
+  echo -e "${red}die${endColor}"
+  echo -e "${yellow}Test: exit container with exit code 1${endColor}"
   exit 1
 }
 export -f die
 
 chaosmonkey() {
-  echo -en "${red}chaosmonkey${endColor}"
-  echo -en "${yellow}TODO Test: stop random running container${endColor}"
+  echo -e "${red}chaosmonkey${endColor}"
+  echo -e "${yellow}TODO Test: stop random running container${endColor}"
   # TODO
 }
 export -f chaosmonkey
 
 passwords() {
-  echo -en "${red}passwords${endColor}"
-  echo -en "${yellow}TODO Test: read password hashes from the host system${endColor}"
+  echo -e "${red}passwords${endColor}"
+  echo -e "${yellow}TODO Test: read password hashes from the host system${endColor}"
   # TODO
 }
 export -f passwords
 
 kernelpanic() {
-  echo -en "${red}kernelpanic${endColor}"
-  echo -en "${yellow}Test: raise kernel panic${endColor}"
+  echo -e "${red}kernelpanic${endColor}"
+  echo -e "${yellow}Test: raise kernel panic${endColor}"
   echo c >/proc/sysrq-trigger
 }
 export -f kernelpanic
